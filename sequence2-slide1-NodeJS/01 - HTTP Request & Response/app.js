6689385dfdcb414a8e61283086dbbfa9d86e05e1
Alexandre Collet <alexandre@collet.email>
2017-03-20 01:44:57 -0700

    Add sequence2 slide1 tp1 correction

---
+++ sequence2-slide1-NodeJS/01 - HTTP Request & Response/app.js
@@ -0,0 +1,76 @@
+/**
+ * Load Modules
+ */
+
+var http = require('http');
+var querystring = require('querystring');
+
+/**
+ * Variables
+ */
+
+var port = 8080;
+
+/**
+ * Create Server
+ */
+
+
+var server = http.createServer(function(request, response) {
+
+    /**
+     * GET /form
+     */
+
+    if(request.url === "/form" && request.method === "GET") {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write("<!doctype html>");
+        response.write("<html><body>");
+        response.write("<form method='POST' action='/completed'>");
+        response.write("<input type='text' name='data1'/>");
+        response.write("<input type='text' name='data2'/>");
+        response.write("<input type='text' name='data3'/>");
+        response.write("<input type='text' name='data4'/>");
+        response.write("<input type='submit' value='Send'/>");
+        response.end("</form></body></html>");
+    }
+
+    /**
+     * POST /completed
+     */
+
+    else if(request.url === "/completed" && request.method === "POST") {
+        var body  = "";
+
+        request.on('data', function(chunck) {
+            body += chunck;
+        });
+
+        request.on('end', function() {
+            response.write(body + "\n\n");
+
+            var data = querystring.parse(body);
+
+            for(var key in data){
+                response.write(key + " ==> " + datas[key] + "\n");
+            }
+
+            response.end();
+        });
+    }
+
+    /**
+     * OTHER
+     */
+
+    else {
+        response.writeHead(403);
+        response.end('Forbidden');
+    }
+
+
+});
+
+server.listen(port);
+
+console.log('Server is ready on port ' + port);
